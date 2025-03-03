3bca9cc8565315c3e25afab4995485e3a84ed059
manuel.garfia <manuel.garfia@106-29.info2>
2025-03-03 11:54:33 +0100

    main

--- MemoryGame/src/game/Main.java
+++ MemoryGame/src/game/Main.java
@@ -1,10 +1,19 @@
 package game;
-
+/**
+ * @author manuel.garfia
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Welcome to the memory game");
+		System.out.println();
+		System.out.println("In the table there's pairs of numbers placed randomly.");
+		System.out.println("You have to select 2 different positions in every turn.");
+		System.out.println("If both positions have the same number, they'll keep undercover.");
+		System.out.println("The objetive is to undercover all the table.");
+		
+		
 	}
 
 }
