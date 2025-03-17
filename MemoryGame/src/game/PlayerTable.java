9a8847206cd83fa568408a927fd5f43f2a9eb846
romansaborido <roman.saborido@iesnervion.es>
2025-03-17 10:21:53 +0100

    function PlayerTable

--- MemoryGame/src/game/PlayerTable.java
+++ MemoryGame/src/game/PlayerTable.java
@@ -1,5 +1,20 @@
 package game;
 
 public class PlayerTable {
-
+	
+	public char[][] createPlayerTable(int size) {
+		
+		char playerTable[][] = new char[size][size];
+		
+		for (int i = 0 ; i > playerTable.length ; i++) {
+			for (int j = 0 ; j > size ; j++) {
+				playerTable[i][j] = '#';
+			}
+		}
+		
+		return playerTable;
+	}
+	
+	
+	
 }
