fd01f988655dbbdbbefc269ca1ed4554890f2112
samuel.jimenez <samuel.jimenez@106-31.info2>
2025-03-17 10:27:42 +0100

    Rama Samuel

--- MemoryGame/src/game/Main.java
+++ MemoryGame/src/game/Main.java
@@ -1,10 +1,38 @@
 package game;
 
+import java.util.Scanner;
+
 public class Main {
+	public static int tableLength;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		String playerName;
+
+		String changeName;
+
+		boolean game = true;
+
+		System.out.println("Welcome to the Memory Game");
+
+		System.out.println("Introduce your Name");
+		playerName = sc.nextLine();
+		System.out.println("Are you sure you want this name? (yes to keep the name no to change it)");
+		changeName = sc.nextLine();
+		if (changeName.equals("yes")) {
+			System.out.println("Your final name is: " + playerName);
+		} else {
+			System.out.println("Introduce your new name");
+			playerName = sc.nextLine();
+		}
+
+		while (game) {
+
+		}
+		
+		sc.close();
 	}
 
 }
